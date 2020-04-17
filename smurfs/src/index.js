d5f7d1332dfe5b62b9fc096191dfbd239b17b806
dawsonhammdev <hammsdesigns@gmail.com>
2020-04-17 10:50:05 -0500

    I have added created my middel ware and store in my index.js src file and wrapped my app in provider

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,30 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// imports for store, provider and middleware
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers'
+import logger from 'redux-logger'
+
+
+//create store and middleware
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(thunk, logger))
+  );
+
+  //wrapping App in provider
+
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootElement
+  );
---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,15 @@
+
+
+// setting up empty reducer and initial state
+const initialState = {
+    smurfs: [],
+    fetchingSmurfs: false,
+    addingSmurfs: false,
+    error: ""
+}
+
+const reducer = (state = initialState, action) => {
+    
+}
+
+export default reducer;
