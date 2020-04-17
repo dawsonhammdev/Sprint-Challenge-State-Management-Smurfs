09c0a526ed87091748194769478a812521cfde78
dawsonhammdev <hammsdesigns@gmail.com>
2020-04-17 11:45:51 -0500

    I have created my reducer logic for get and post

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,3 +1,12 @@
+//import actions
+import {
+    FETCH_SMURFS_START,
+    FETCH_SMURFS_SUCCESS,
+    FETCH_SMURFS_FAIL,
+    POST_SMURF_SUCCESS,
+    POST_SMURF_FAIL,
+} from '../actions'
+
 
 
 // setting up empty reducer and initial state
@@ -9,7 +18,43 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    
-}
+    switch (action.type) {
+        case FETCH_SMURFS_START: 
+        return {
+            ...state,
+            fetchingSmurfs: true
+        };
+        case FETCH_SMURFS_SUCCESS:
+            return {
+                ...state,
+                smurfs: action.payload,
+                fetchingSmurfs: false,
+                error: ""
+            };
+        case FETCH_SMURFS_FAIL: 
+            return {
+                ...state,
+                fetchingSmurfs: false,
+                error: action.payload
+            };
+        case POST_SMURF_SUCCESS: 
+            const newSmurf = {
+                name: action.payload,
+                age: "",
+                height: "" 
+            };
+            return { ...state, smurfs: [...state.smurfs, newSmurf] };
+        case POST_SMURF_FAIL:
+            return {
+                ...state,
+                addingSmurfs: false,
+                error: action.payload,
+                fetchingSmurfs: false
+            };
+
+        default: 
+            return state;
+    };
+};
 
 export default reducer;
