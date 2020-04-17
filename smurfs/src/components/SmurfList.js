00a9558906ae966e6c559b5fe4b61face064761e
dawsonhammdev <hammsdesigns@gmail.com>
2020-04-17 10:58:47 -0500

    Created SmurfList function in smurflist.js and connected it.

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { connect } from "react-redux";
+
+const SmurfList = props => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default connect()(SmurfList)
+
+
