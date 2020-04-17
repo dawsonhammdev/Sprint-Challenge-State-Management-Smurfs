200315011b171f369c979892d73adcedfb3d69d1
dawsonhammdev <hammsdesigns@gmail.com>
2020-04-17 12:11:24 -0500

    I have added my smurf to the app and it WORKS!!!

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,15 +1,43 @@
 import React from "react"
 import { connect } from "react-redux";
+import { getSmurfs } from "../actions";
+
 
 const SmurfList = props => {
     return (
         <div>
+            <button onClick={() => props.getSmurfs()}>
+            POPULATE SMURF
+            </button>
 
+        {props.smurfs.map(smurf => (
+            <div key = {smurf.id}>
+                <h2>Name: {smurf.name}</h2>
+                <p>Age: {smurf.age}</p>
+                <p>Height: {smurf.height}</p>
+            </div>
+        
+        ))}
         </div>
     )
 }
 
+const mapStateToProps = state => {
+    return {
+        smurfs: state.smurfs,
+        fetchingSmurfs: state.fetchingSmurfs,
+        error: state.error
+    };
+};
+
 // connecting SmurfList
-export default connect()(SmurfList)
+
+export default connect(
+    mapStateToProps,
+    { getSmurfs }
+)(SmurfList);
+
+
+
 
 
